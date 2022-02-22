645a15aec43b2ec61502fd55e63a67d8f2c68f2e
ma7762la-s <majjalarsen@hotmail.com>
2022-02-22 16:47:18 +0000

    Latest changes
    yes

---
+++ Assignment3HyggeSolution/Assignment3Hygge/ParamArgs.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment3Hygge 
+{
+    public class ParamArgs
+    {
+        public ParamArgs(string paramID, object val) { ParamID = paramID; Value = val; } //constructor
+        public string ParamID { get; set; } //ParamID property
+        public object Value { get; set; } //Value property
+    }
+}
