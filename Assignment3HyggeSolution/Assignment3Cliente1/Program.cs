09d336a2355b0266556ba40dceec55ab0a0e15b6
rebeccakejlberg <rebecca.kejlberg1@gmail.com>
2022-02-23 17:10:29 +0100

    new

--- Assignment3HyggeSolution/Assignment3Cliente1/Program.cs
+++ Assignment3HyggeSolution/Assignment3Cliente1/Program.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿
+
+using System;
+using System.Data;
+using System.Data.SqlClient;
 
 
 namespace Assignment3Cliente1
@@ -7,15 +11,50 @@ namespace Assignment3Cliente1
     {
         static void Main(string[] args)
         {
-            try
+            
             {
                 HyggeService client = new HyggeService();
+
+
+
+                Console.WriteLine("---------Meny-------" + '\n' + "-----Please Enter a One Of Following ------"
+                    + '\n' + "Person, Industry, Login, Intrest, Education or Relationship");
+
+                string response = Console.ReadLine();
+                DataSet ds = new DataSet();
+
+                while (response != "Exit")
+
+                    switch (response.ToLower())
+                    {
+                        case "log":
+                            ds = DataAccessLayer.Utils.ViewAll(Table.Logins);
+                            break;
+
+                        case "pers":
+                            ds = DataAccessLayer.Utils.ViewAll(Table.Person);
+                            break;
+
+                        case "rela":
+                            ds = DataAccessLayer.Utils.ViewAll(Table.Relationship);
+                            break;
+
+                        case "int":
+                            ds = DataAccessLayer.Utils.ViewAll(Table.Interest);
+                            break;
+
+                        case "ind":
+                            ds = DataAccessLayer.Utils.ViewAll(Table.Industry);
+                            break;
+
+                        case "edu":
+                            ds = DataAccessLayer.Utils.ViewAll(Table.Education);
+                            break;
+
+                    }
             }
-            Console.WriteLine("---------Meny-------" + '\n' + "-----Please enter a One of Following ------"
-                + '\n' + "Person, Industry, Login, Intrest, Education or Relationship");
-
-
-
+           
+            }
         }
     }
-}
+            
