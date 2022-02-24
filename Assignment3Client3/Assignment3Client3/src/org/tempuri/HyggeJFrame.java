2fdbbfb1e23563649d498429edfeb7f50d2b3457
rebeccakejlberg <rebecca.kejlberg1@gmail.com>
2022-02-24 15:36:09 +0100

    git

--- Assignment3Client3/Assignment3Client3/src/org/tempuri/HyggeJFrame.java
+++ Assignment3Client3/Assignment3Client3/src/org/tempuri/HyggeJFrame.java
@@ -7,21 +7,36 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
-public class HyggeFrame extends JFrame {
+public class HyggeJFrame extends JFrame {
 
 	private JPanel contentPane;
 
 	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					HyggeJFrame frame = new HyggeJFrame();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
 	 * Create the frame.
 	 */
-	public HyggeFrame() {
+	public HyggeJFrame() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
-		
 	}
 
 }
